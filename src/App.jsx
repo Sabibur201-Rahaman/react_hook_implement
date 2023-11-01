504338d4a6e6e0e9a9ce63ab2f8f41d712a5ccdf
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-01 13:14:24 +0530

    useState Working With multiple  Immutable Object

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ function App() {
     key3: 'value3',
   });
   const change = () => {
-    setMyObject({
-      key1: 'newValue1',
-      key2: 'value1',
-      key3: 'value2',
-      key4: 'value3',
-      key5: 'mainValue3',
-    });
+    setMyObject(
+      prevObj=>({
+        ...prevObj,
+        key1:'Ostad platform',
+        key2:'javascript',
+        key3:'python'
+      })
+    );
   };
   return (
     <>
     <p>{myObject.key1}</p>
+    <p>{myObject.key2}</p>
+    <p>{myObject.key3}</p>
       <button onClick={change}>click</button>
     </>
   );

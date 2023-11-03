1b2a6ea55f1a0a636511152fa82117b0bff7bba1
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-03 21:23:33 +0530

    use of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,14 @@
-import React, { useState } from "react";
-
+import React from 'react'
+import { useEffect } from 'react'
 function App() {
-  let [formObject, setFormObject] = useState({
-    fName: "",
-    lName: "",
-    city: "",
-    gender: "Male",
-  });
-  const inputOnChange = (property, value) => {
-    setFormObject((prevObj) => ({
-      ...prevObj,
-      [property]:value
-    }));
-  };
-
-  const formOnSubmit=(e)=>{
-    e.preventDefault()
-    // console.log(formObject)
-    alert(JSON.stringify(formObject))
-  }
+  useEffect(()=>{
+console.log('heloo')
+  },[4])
   return (
-    <div className="container">
-      <form onSubmit={formOnSubmit}>
-        <input onChange={(e)=>{inputOnChange("fName",e.target.value)}} value={formObject.fName} placeholder="firstName" />
-        <br />
-        <input onChange={(e)=>{inputOnChange("lName",e.target.value)}} value={formObject.lName} placeholder="lastName" />
-        <br />
-        <select onChange={(e)=>{inputOnChange("city",e.target.value)}} value={formObject.city}>
-          <option value="">chose city</option>
-          <option value="Dhaka">Dhaka</option>
-          <option value="">Rangpur</option>
-        </select>
-        <br />
-        <input onChange={()=>{inputOnChange("gender","Male")}}
-          checked={formObject.gender === "Male"}
-          type="radio"
-          name="gender"
-        />
-        Male
-        <input onChange={()=>{inputOnChange("gender","Female")}}
-          checked={formObject.gender === "Female"}
-          type="radio"
-          name="gender"
-        />
-        Female
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div>
+      <h1>this is react effect</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'milligram'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>
 )

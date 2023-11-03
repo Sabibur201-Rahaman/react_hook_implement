388d5a53fb2d728344e416794cd650b226238855
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-03 16:24:16 +0530

    useState Managing Form Like Pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
-import { useRef, useState } from "react";
-import Hello from "./hello";
+import React, { useState } from "react";
 
 function App() {
-  const [list, setList] = useState([]);
-  const [item, setItem] = useState("");
-  const addToList = () => {
-    list.push(item);
-    setList([...list]);
+  let [formObject, setFormObject] = useState({
+    fName: "",
+    lName: "",
+    city: "",
+    gender: "Male",
+  });
+  const inputOnChange = (property, value) => {
+    setFormObject((prevObj) => ({
+      ...prevObj,
+      [property]:value
+    }));
   };
-  const removeItem=(index)=>{
-    list.splice(index,1)
-    setList([...list])
+
+  const formOnSubmit=(e)=>{
+    e.preventDefault()
+    // console.log(formObject)
+    alert(JSON.stringify(formObject))
   }
   return (
-    <>
-    <Hello/>
-      {/* <p>{item}</p>
-    <p>{list.length}</p> */}
-      <table>
-        <tbody>
-          {list.length !== 0 ? (
-            list.map((element, index) => {
-              return (
-                <tr>
-                  <td>{element}</td>
-                  <td>
-                    <button onClick={()=>{
-                      removeItem(index)
-                    }}>Remove</button>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr></tr>
-          )}
-        </tbody>
-      </table>
-
-      <input onChange={(e) => setItem(e.target.value)} placeholder="item" />
-      <button onClick={addToList}>click</button>
-    </>
+    <div className="container">
+      <form onSubmit={formOnSubmit}>
+        <input onChange={(e)=>{inputOnChange("fName",e.target.value)}} value={formObject.fName} placeholder="firstName" />
+        <br />
+        <input onChange={(e)=>{inputOnChange("lName",e.target.value)}} value={formObject.lName} placeholder="lastName" />
+        <br />
+        <select onChange={(e)=>{inputOnChange("city",e.target.value)}} value={formObject.city}>
+          <option value="">chose city</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="">Rangpur</option>
+        </select>
+        <br />
+        <input onChange={()=>{inputOnChange("gender","Male")}}
+          checked={formObject.gender === "Male"}
+          type="radio"
+          name="gender"
+        />
+        Male
+        <input onChange={()=>{inputOnChange("gender","Female")}}
+          checked={formObject.gender === "Female"}
+          type="radio"
+          name="gender"
+        />
+        Female
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 

4b98130c13827e7aecabcdb8e56074ff053a86db
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-05 18:37:34 +0530

    Navlink used in reactRouterDom

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Menu() {
   return (
     <div>
       <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/product'>Product</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/product'>Product</NavLink></li>
+        <li><NavLink to='/profile'>Profile</NavLink></li>
       </ul>
     </div>
   );
